a54bae47bc7d82ddee8bf2567fe9e90ed64ab7b5
Alley Stoughton <alley.stoughton@icloud.com>
2020-06-11 17:42:21 -0400

    More fixes.

--- uc-dsl/LexerParser/_build/dlCheck.ml
+++
@@ -1,7 +0,0 @@
-open DlParseFile
-open DlUtils
-
-let () = Printexc.record_backtrace true
-
-let () = try ignore (parse_file Sys.argv.(1)) with exn -> printEx exn
-
--- uc-dsl/LexerParser/_build/dlExpressions.mli
+++
@@ -1,7 +0,0 @@
-open DlParseTree
-open DlParsedTree
-open DlTypes
-
-val checkExpression: (qid->typ)->expressionL->typ
-val isDistribution: typ->bool
-val getDistrubutionTyp: typ->typ
