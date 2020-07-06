6cbbcabbd3b1a77ed7036ba0cbf1578239cec188
Alley Stoughton <alley.stoughton@icloud.com>
2020-07-06 10:55:28 -0400

    Fixed .merlin file.

--- uc-dsl/src/ucEcTypes.ml
+++ uc-dsl/src/ucEcTypes.ml
@@ -4,13 +4,15 @@ open UcTypes
 open UcSpec
 open EcLocation
 
-let checkNamedType (tyname:id) : typ =
-	let tyn = unloc tyname in
-	try ignore (List.find (fun tyn -> tyn=unloc tyname) builtinTypeNames); Tconstr (tyn,None)
-	with Not_found ->
-		if UcEcInterface.existsType tyn then Tconstr (tyn,None)
-		else parse_error (loc tyname) (Some ("Non-existing type: "^(tyn)) )
-let rec checkType (ty:ty) : typ =
-	match ty with
-	| NamedTy id -> checkNamedType id
-	| TupleTy tl -> Ttuple (List.map (fun t ->checkType t) tl)
+let checkNamedType (tyname : id) : typ =
+  let tyn = unloc tyname in
+  try ignore (List.find (fun tyn' -> tyn' = tyn) builtinTypeNames);
+      Tconstr (tyn, None) with
+    Not_found ->
+      if UcEcInterface.existsType tyn then Tconstr (tyn, None)
+      else parse_error (loc tyname) (Some ("Non-existing type: " ^ tyn))
+
+let rec checkType (ty : ty) : typ =
+  match ty with
+  | NamedTy id -> checkNamedType id
+  | TupleTy tl -> Ttuple (List.map (fun t -> checkType t) tl)
