6bb6b1d46c536f52113d6448a145f8bfce025b4f
Alley Stoughton <alley.stoughton@icloud.com>
2020-07-11 11:02:06 -0400

    Tweak to error message format.

--- uc-dsl/src/ucMessage.ml
+++ uc-dsl/src/ucMessage.ml
@@ -17,7 +17,7 @@ let loc_to_str_raw (p : EcLocation.t) =
 
 let message res mt filename loco msg =
   let mt_str = message_type_str mt in
-  let raw = UcState.get_raw_messages() in
+  let raw = UcState.get_raw_messages () in
   if raw then
     match loco with
     | None     ->
@@ -29,12 +29,12 @@ let message res mt filename loco msg =
          "%s: %s %s\n%s\n" mt_str filename loc_str msg; res ())
   else match loco with
        | None     ->
-           (Printf.fprintf stderr "[%s: %s] %s\n" mt_str filename msg; res ())
+           (Printf.fprintf stderr "[%s: %s]\n%s\n" mt_str filename msg; res ())
        | Some loc ->
            let loc_str = EcLocation.tostring loc in
            (Printf.fprintf
             stderr 
-            "[%s: %s%s] %s\n" mt_str filename loc_str msg; res ())
+            "[%s: %s%s]\n%s\n" mt_str filename loc_str msg; res ())
 
 let error_message = message (fun () -> exit 1) ErrorMessage
 
