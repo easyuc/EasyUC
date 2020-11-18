badd54bfbc0051a98a6b66c7923fd664ab69b099
Alley Stoughton <alley.stoughton@icloud.com>
2020-11-18 18:15:06 -0500

    Removing old files.

--- uc-dsl/src/old/ucExpressions.mli
+++
@@ -1,8 +0,0 @@
-(* UcExpressions module interface *)
-
-open UcTypes
-open UcSpec
-
-val check_expression : (qid -> typ) -> expression -> typ
-val is_distribution : typ -> bool
-val get_distribution_typ : typ -> typ
