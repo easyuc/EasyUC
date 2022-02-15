3f38a0b9e09bad13192e72b7f6b246087c0f3a6e
01tomislav <tomislav@bu.edu>
2022-02-15 05:44:05 -0500

    writing axiom _adv_if_pi_gt0

--- uc-dsl/ucdsl-proj/src/ucGenerateEcFile.ml
+++ uc-dsl/ucdsl-proj/src/ucGenerateEcFile.ml
@@ -11,23 +11,47 @@ let open_theory (name : string) : unit =
 let close_theory (name : string) : unit =
   UcEcInterface.process (GthClose ([], dl name))
 
-let operator (pop : poperator) : unit =
+let decl_operator (pop : poperator) : unit =
   UcEcInterface.process (Goperator pop)
+  
+let decl_axiom (pax : paxiom) : unit =
+  UcEcInterface.process (Gaxiom pax)
+  
+let print_newline (ppf : Format.formatter) : unit =
+  Format.fprintf stf "@."; (*REMOVE*)
+  Format.fprintf ppf "@."
   
 let print_theory (ppf : Format.formatter) (name : string) : unit = 
   let env = UcEcInterface.env () in
   let ppe = EcPrinting.PPEnv.ofenv env in
   let pth = EcEnv.Theory.lookup ([], name) env in
   EcPrinting.pp_theory ppe stf pth; (*REMOVE*)
-  EcPrinting.pp_theory ppe ppf pth
+  EcPrinting.pp_theory ppe ppf pth;
+  print_newline ppf
+
+let print_operator (ppf : Format.formatter) (name : string) : unit = 
+  let env = UcEcInterface.env () in
+  let ppe = EcPrinting.PPEnv.ofenv env in
+  let op = EcEnv.Op.lookup ([], name) env in
+  EcPrinting.pp_opdecl ppe stf op; (*REMOVE*)
+  EcPrinting.pp_opdecl ppe ppf op;
+  print_newline ppf
   
-let pi_op : poperator =
+let print_axiom (ppf : Format.formatter) (name : string) : unit = 
+  let env = UcEcInterface.env () in
+  let ppe = EcPrinting.PPEnv.ofenv env in
+  let ax = EcEnv.Ax.lookup ([], name) env in
+  EcPrinting.pp_axiom ppe stf ax; (*REMOVE*)
+  EcPrinting.pp_axiom ppe ppf ax;
+  print_newline ppf
+  
+let abs_oper_int (name : string) : poperator =  
   let pq_int = ([],"int") in
   let pty_int = PTnamed (dl pq_int) in
   let podef = PO_abstr (dl pty_int) in
   {
     po_kind     = `Op;
-    po_name     = dl "pi";
+    po_name     = dl name;
     po_aliases  = [];
     po_tags     = [];
     po_tyvars   = None;
@@ -38,11 +62,32 @@ let pi_op : poperator =
     po_locality = `Global;
   }
   
+let pi_op : poperator = abs_oper_int "pi"
+
+let opname_adv_if_pi = "_adv_if_pi"
+
+let axname_adv_if_pi_gt0 = "_adv_if_pi_gt0"
+
+let axiom_adv_if_pi_gt0 : paxiom =
+  let f_le = dl (PFident (dl ([], "<"), None)) in
+  let f_int = dl (PFint EcBigInt.zero) in
+  let f_ax = dl (PFident (dl ([], opname_adv_if_pi), None)) in 
+  let pfrm = dl (PFapp (f_le,[f_int; f_ax])) in 
+  {
+    pa_name     = dl axname_adv_if_pi_gt0;
+    pa_tyvars   = None;
+    pa_vars     = None;
+    pa_formula  = pfrm;
+    pa_kind     = PAxiom [];
+    pa_nosmt    = false;
+    pa_locality = `Global;
+  }
+  
 let write_message (name : string) (mb : message_body_tyd) : unit = ()
   
 let write_basic_dir_int (ppf : Format.formatter) (name : string) (bibt : basic_inter_body_tyd) : unit =
   open_theory name;
-  operator pi_op;
+  decl_operator pi_op;
   IdMap.iter (fun n mb -> write_message n mb) bibt;
   close_theory name;
   print_theory ppf name
@@ -67,10 +112,20 @@ let write_require_import_UCBasicTypes (ppf : Format.formatter) : unit =
      Some `Import) in
   UcEcInterface.process (GthRequire threq);
   Format.fprintf stf "@[require import UCBasicTypes.@]@."; (*REMOVE*)
-  Format.fprintf ppf "@[require import UCBasicTypes.@]@." 
+  Format.fprintf ppf "@[require import UCBasicTypes.@]@."
+  
+let write_op_adv_if_pi (ppf : Format.formatter) : unit =
+  decl_operator (abs_oper_int opname_adv_if_pi);
+  print_operator ppf opname_adv_if_pi
+
+let write_ax_adv_if_pi_gt0 (ppf : Format.formatter) : unit =
+  decl_axiom (axiom_adv_if_pi_gt0);
+  print_axiom ppf axname_adv_if_pi_gt0
      
 let write_file (ppf : Format.formatter) (sts : singlefile_typed_spec) : unit =
   write_require_import_UCBasicTypes ppf;
+  write_op_adv_if_pi ppf;
+  write_ax_adv_if_pi_gt0 ppf;
   IdMap.iter (fun n d -> write_dir_int ppf n d) sts.dir_inter_map
 
 (*---------------------------------------------------------------------------*)
