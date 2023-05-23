945a5315869a8e67fde67a378d940b09036bb4b6
01tomislav <01tomislav.petrovic@gmail.com>
2023-05-23 15:02:21 +0200

    testing of fun_expr_to_worlds

--- uc-dsl/ucdsl-proj/src/test0_of_UcInterpreter.ml
+++ uc-dsl/ucdsl-proj/src/test0_of_UcInterpreter.ml
@@ -27,6 +27,44 @@ let w =
   worlds_ideal = iw;
 }
 
+let test_worlds_pp_0 () =
+  pp_worlds Format.std_formatter w
+  
+(********)
 
+let parse_error_handling lexbuf =
+ UcMessage.error_message  (* no need to close channel *)
+ (EcLocation.make lexbuf.Lexing.lex_start_p lexbuf.Lexing.lex_curr_p)
+ (fun ppf -> Format.fprintf ppf "@[parse@ error@]")
+
+let parse_fun_expr (fe : string) : UcSpec.fun_expr =
+  let lexbuf = Lexing.from_string fe in
+  try UcParser.fun_expr UcLexer.read lexbuf  with
+  | UcParser.Error -> parse_error_handling lexbuf
+
+let test_fun_expr_to_worlds (include_dirs : string list) (file : string) (fun_ex : string) : unit =
+  UcState.set_units();
+  UcState.set_include_dirs include_dirs;
+  let maps = UcParseAndTypecheckFile.parse_and_typecheck_file_or_id (FOID_File file) in
+  let root = UcUtils.capitalized_root_of_filename_with_extension file in
+  let fun_expr = parse_fun_expr fun_ex in
+  let w = fun_expr_to_worlds root maps fun_expr in
+  pp_worlds Format.std_formatter w
+
+(* include dirs not used when opening file! 
+test has to be run in directory that contains SMC.uc file!*)
+let smc_dir = "~/EasyUC/uc-dsl/examples/smc-case-study"
+let smc = "SMC.uc"
+
+let test_fun_expr_to_worlds_0 (): unit =
+  let fe = "SMC.SMCReal(KeyExchange.KEReal())" in
+  test_fun_expr_to_worlds [smc_dir] smc fe
+
+(*********)
+  
 let () =
-  pp_worlds Format.std_formatter w 
+  test_worlds_pp_0 ();
+  print_endline "";
+  UcEcInterface.init ();
+  test_fun_expr_to_worlds_0 ();
+  print_endline ""
