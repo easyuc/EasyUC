39bdfe7040438ceaa47146523e38132af161d87b
01tomislav <01tomislav.petrovic@gmail.com>
2023-09-28 16:10:21 +0200

    changed printing of effects in ucInterpreter. fixes #15

--- uc-dsl/ucdsl-proj/src/ucInterpreterClient.ml
+++ uc-dsl/ucdsl-proj/src/ucInterpreterClient.ml
@@ -101,27 +101,49 @@ let pp_uc_file_pos
 
 let pp_interpreter_state 
 (fmt : Format.formatter) ( c : interpreter_state) : unit =
-    match c.config with
-    | Some config ->
-      Format.fprintf fmt "%a" pp_config config;
-      begin match c.effect with
-      | None -> ()
-      | Some eff ->
-        Format.fprintf fmt "@.%a" 
-        pp_effect eff;
-      end
-    | None ->
-      match c.config_gen with
-      | Some config -> 
-        Format.fprintf fmt "%a" pp_config config
-      | None -> 
-        match c.maps with
-        | Some _ -> 
-          let str = "uc file "^(Option.get c.root)^" loaded" in
-          Format.fprintf fmt "%s" str
-        | None   ->
-          let str = "uc file not loaded" in
-          Format.fprintf fmt "%s" str
+  let pp_effect (ppf : Format.formatter) (e : effect) : unit =
+    let pp_control (ppf : Format.formatter) (ctrl : control) : unit =
+      match ctrl with
+      | CtrlEnv -> Format.fprintf ppf "environment"
+      | CtrlAdv -> Format.fprintf ppf "adversary"
+    in
+    match e with
+    | EffectOK                       -> ()
+    | EffectRand id                  ->
+      Format.fprintf ppf "@[Note: random value was assigned to: %s@]" id
+    | EffectMsgOut (pp_sme, ctrl)    ->
+      Format.fprintf ppf 
+      "@[Message was output:@ %a:@ %s@]" pp_control ctrl pp_sme
+    | EffectFailOut                  -> 
+      Format.fprintf ppf "Note: \"fail.\" was called."
+    | EffectBlockedIf                -> 
+      Format.fprintf ppf "Blocking: cannot decide if condition."
+    | EffectBlockedMatch             -> 
+      Format.fprintf ppf "Blocking: cannot decide matching condition."
+    | EffectBlockedPortOrAddrCompare ->
+      Format.fprintf ppf "Blocking: cannot decide port or addr comparison."
+  in
+  match c.config with
+  | Some config ->
+    Format.fprintf fmt "%a" pp_config config;
+    begin match c.effect with
+    | None -> ()
+    | Some eff ->
+      Format.fprintf fmt "@.%a" 
+      pp_effect eff;
+    end
+  | None ->
+    match c.config_gen with
+    | Some config -> 
+      Format.fprintf fmt "%a" pp_config config
+    | None -> 
+      match c.maps with
+      | Some _ -> 
+        let str = "uc file "^(Option.get c.root)^" loaded" in
+        Format.fprintf fmt "%s" str
+      | None   ->
+        let str = "uc file not loaded" in
+        Format.fprintf fmt "%s" str
 
 let interpret (lexbuf : L.lexbuf) =
 
